d515ca1f4c59e6dafce7ce318420fadd772e19a8
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-04-17 15:09:59 -0600

    I create the footer

---
+++ src/components/Home/FooterHome.tsx
@@ -0,0 +1,24 @@
+export default function FooterHome() {
+  return (
+    <>
+      <div className="container text-center">
+        <div className="row align-items-end">
+          <div className="col">
+            Phone Number
+            <div>(xxx)-xxxx-xxxx</div>
+          </div>
+          <div className="col">
+            Email
+            <div>
+              example@gmail.com
+            </div>
+          </div>
+          <div className="col">
+            Address
+            <div>4 street example zone number example</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
