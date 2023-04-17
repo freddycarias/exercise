8ef22d5cdc8c59d5dbbf99fbf5de4079298d4b8b
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-04-17 15:08:48 -0600

    I change the word our to my

--- src/components/Home/HeaderHome.tsx
+++ src/components/Home/HeaderHome.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="container text-center">
       <div className="row justify-content-end" style={{paddingTop: "5px"}}>
         <div className="col-4">
-          Our Web
+          My Web
         </div>
         <div className="col-4" style={{ textAlign: "right"}}>
           <Button btn={"btn btn-outline-success"} route={'/undefine1'} inside={"Log in"}/>
