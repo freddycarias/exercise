e9ac2b3bded37b4a3828ba40b6c81d35c26fb290
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-04-17 15:11:12 -0600

    I added body and footer in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,14 @@
+import BodyHome from "../components/Home/BodyHome";
+import FooterHome from "../components/Home/FooterHome";
 import HeaderHome from "../components/Home/HeaderHome";
 
 export default function Home() {
   return (
+    <>
     <HeaderHome/>
+    <BodyHome/>
+    <FooterHome/>
+    </>
   )
 }
 
