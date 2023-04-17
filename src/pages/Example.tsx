6340dd3aaabd3517c7064935bda0b59de8b9cf26
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-04-17 17:06:20 -0600

    I create example because I needed to test the header and footer

---
+++ src/pages/Example.tsx
@@ -0,0 +1,11 @@
+import Footer from "../components/GeneralHeader&&Footer/Footer";
+import Header from "../components/GeneralHeader&&Footer/Header";
+
+export default function Example() {
+  return (
+    <div className="container text-center">
+        <Header title="Example"/>
+        <Footer/>
+    </div>
+  )
+}
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Example from "../pages/Example";
 
 export default function RoutesPage() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/example" element={<Example/>}/>
         </Routes>
     </BrowserRouter>
   )
