d93897a59c3fe8e8cd6a8dfea9f6dabf3e773228
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-04-18 16:24:34 -0600

    I create the main page

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/GeneralHeader&&Footer/Header";
+import Footer from "../components/GeneralHeader&&Footer/Footer";
+import BodyHome from "../components/Home/BodyHome";
+
+export default function MainPage() {
+  return (
+    <>
+    <div className="container text-center">
+        <Header/>
+        <BodyHome/>
+        <Footer/>
+    </div>
+    </>
+  )
+}
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,6 +3,7 @@ import Home from "../pages/Home";
 import Example from "../pages/Example";
 import LogIn from "../pages/LogIn";
 import CheckIn from "../pages/CheckIn";
+import MainPage from "../pages/MainPage";
 
 export default function RoutesPage() {
   return (
@@ -11,6 +12,7 @@ export default function RoutesPage() {
             <Route path="/" element={<Home/>}/>
             <Route path="/login" element={<LogIn/>}/>
             <Route path="/checkin" element={<CheckIn/>}/>
+            <Route path="/main" element={<MainPage/>}/>
             <Route path="/example" element={<Example/>}/>
         </Routes>
     </BrowserRouter>
