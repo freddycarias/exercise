1cd7e27524d36577932aa3e5343b219deb73a817
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-04-18 10:32:54 -0600

    test file just to see my pages before implementing the new changes.

--- src/pages/Example.tsx
+++ src/pages/Example.tsx
@@ -1,11 +1,17 @@
-import Footer from "../components/GeneralHeader&&Footer/Footer";
 import Header from "../components/GeneralHeader&&Footer/Header";
+import LogIn from "./LogIn";
+import CheckIn from "./CheckIn";
+import Footer from "../components/GeneralHeader&&Footer/Footer";
 
 export default function Example() {
   return (
+    <>
     <div className="container text-center">
         <Header title="Example"/>
+        <LogIn/>
+        <CheckIn/>
         <Footer/>
     </div>
+    </>
   )
 }
