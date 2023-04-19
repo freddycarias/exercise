ae2bcc24034b96295170be2d8368505da9ca9331
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-04-19 12:58:19 -0600

    I create CardsBody because are of using global

---
+++ src/components/Card/CardsBody.tsx
@@ -0,0 +1,42 @@
+const Cards = () => {
+  return (
+    <>
+      <div className="col">
+        <div className="p-3">
+          <div className="card" style={{ width: "18rem" }}>
+            <img src="https://media.cnn.com/api/v1/images/stellar/prod/allbirds-sneakers-review-wool-runnerjpg.jpg?q=h_1090,w_1938,x_0,y_0" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card with stretched link</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary stretched-link">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+export default function CardsBody() {
+  return (
+    <div>
+      <div className="container px-4 text-center">
+        <div className="row gx-5">
+          <Cards/>
+          <Cards/>
+          <Cards/>
+          <Cards/>
+          <Cards/>
+          <Cards/>
+        </div>
+      </div>
+    </div>
+  );
+}
+  
