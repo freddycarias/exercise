6880f4012de81dfc2a5ce3849276cebc6ca88ba5
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-04-19 16:03:17 -0600

    I create the cards and I added their images

---
+++ src/components/BodyCards/Watches/BodyWatchesF.tsx
@@ -0,0 +1,18 @@
+import { CardsBody } from "../../Card/CardsBody";
+
+export default function BodyWatchesF() {
+  return (
+    <div>
+      <div className="container px-4 text-center">
+        <div className="row gx-5">
+          <CardsBody src={"https://d2chqqff64diva.cloudfront.net//eyJidWNrZXQiOiJjbXMuc3RyYXBpLmZlc3RpbmFncm91cCIsImtleSI6InJlbG9qZXNfbXVqZXJfMDM5ZjYzMDdjZi5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0IjoiaW5zaWRlIn19fQ=="}/>
+          <CardsBody src={"https://d2chqqff64diva.cloudfront.net//eyJidWNrZXQiOiJjbXMuc3RyYXBpLmZlc3RpbmFncm91cCIsImtleSI6InJlbG9qZXNfbXVqZXJfMDM5ZjYzMDdjZi5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0IjoiaW5zaWRlIn19fQ=="}/>
+          <CardsBody src={"https://d2chqqff64diva.cloudfront.net//eyJidWNrZXQiOiJjbXMuc3RyYXBpLmZlc3RpbmFncm91cCIsImtleSI6InJlbG9qZXNfbXVqZXJfMDM5ZjYzMDdjZi5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0IjoiaW5zaWRlIn19fQ=="}/>
+          <CardsBody src={"https://d2chqqff64diva.cloudfront.net//eyJidWNrZXQiOiJjbXMuc3RyYXBpLmZlc3RpbmFncm91cCIsImtleSI6InJlbG9qZXNfbXVqZXJfMDM5ZjYzMDdjZi5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0IjoiaW5zaWRlIn19fQ=="}/>
+          <CardsBody src={"https://d2chqqff64diva.cloudfront.net//eyJidWNrZXQiOiJjbXMuc3RyYXBpLmZlc3RpbmFncm91cCIsImtleSI6InJlbG9qZXNfbXVqZXJfMDM5ZjYzMDdjZi5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0IjoiaW5zaWRlIn19fQ=="}/>
+          <CardsBody src={"https://d2chqqff64diva.cloudfront.net//eyJidWNrZXQiOiJjbXMuc3RyYXBpLmZlc3RpbmFncm91cCIsImtleSI6InJlbG9qZXNfbXVqZXJfMDM5ZjYzMDdjZi5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsiZml0IjoiaW5zaWRlIn19fQ=="}/>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/BodyCards/Watches/BodyWatchesM.tsx
@@ -0,0 +1,18 @@
+import { CardsBody } from "../../Card/CardsBody";
+
+export default function BodyWatchesM() {
+  return (
+    <div>
+      <div className="container px-4 text-center">
+        <div className="row gx-5">
+          <CardsBody src={"https://media.gq.com.mx/photos/63471cd850c7ecd84ad8790b/16:9/w_2560%2Cc_limit/relojes-de-colores-para-look-casual-de-hombre.png"}/>
+          <CardsBody src={"https://media.gq.com.mx/photos/63471cd850c7ecd84ad8790b/16:9/w_2560%2Cc_limit/relojes-de-colores-para-look-casual-de-hombre.png"}/>
+          <CardsBody src={"https://media.gq.com.mx/photos/63471cd850c7ecd84ad8790b/16:9/w_2560%2Cc_limit/relojes-de-colores-para-look-casual-de-hombre.png"}/>
+          <CardsBody src={"https://media.gq.com.mx/photos/63471cd850c7ecd84ad8790b/16:9/w_2560%2Cc_limit/relojes-de-colores-para-look-casual-de-hombre.png"}/>
+          <CardsBody src={"https://media.gq.com.mx/photos/63471cd850c7ecd84ad8790b/16:9/w_2560%2Cc_limit/relojes-de-colores-para-look-casual-de-hombre.png"}/>
+          <CardsBody src={"https://media.gq.com.mx/photos/63471cd850c7ecd84ad8790b/16:9/w_2560%2Cc_limit/relojes-de-colores-para-look-casual-de-hombre.png"}/>
+        </div>
+      </div>
+    </div>
+  );
+}
