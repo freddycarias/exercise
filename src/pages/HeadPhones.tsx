498a1b5b7b7df8ebb27c4d6e1d3cef23d1ea8d16
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-04-19 14:53:34 -0600

    I create HeadPhones and I added Header, Body and Footer

---
+++ src/pages/HeadPhones.tsx
@@ -0,0 +1,15 @@
+import BodyHeadphones from "../components/BodyCards/HeadPhones/BodyHeadphones";
+import Footer from "../components/GeneralHeader&&Footer/Footer/Footer";
+import Header from "../components/GeneralHeader&&Footer/Header/Header";
+
+export default function HeadPhones() {
+  return (
+    <>
+    <div className="container text-center">
+      <Header/>
+      <BodyHeadphones/>
+      <Footer/>
+    </div>
+  </>
+  )
+}
