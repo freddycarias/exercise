1e4eba6c6d719af215a87778fce948b2a8fcdfe6
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-04-19 16:01:03 -0600

    I create the routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -6,6 +6,9 @@ import CheckIn from "../pages/CheckIn";
 import MainPage from "../pages/MainPage";
 import Phone from "../pages/Phones";
 import HeadPhones from "../pages/HeadPhones";
+import Masculine from "../pages/Masculine";
+import Femenine from "../pages/Femenine";
+import Unixes from "../pages/Unixes";
 
 export default function RoutesPage() {
   return (
@@ -17,6 +20,9 @@ export default function RoutesPage() {
             <Route path="/main" element={<MainPage/>}/>
             <Route path="/phones" element={<Phone/>}/>
             <Route path="/headphones" element={<HeadPhones/>}/>
+            <Route path="/masculine" element={<Masculine/>}/>
+            <Route path="/femenine" element={<Femenine/>}/>
+            <Route path="/unixes" element={<Unixes/>}/>
             <Route path="/example" element={<Example/>}/>
         </Routes>
     </BrowserRouter>
