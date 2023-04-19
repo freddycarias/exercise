bbe2647c7cb2f3d97ca2a73f52738af02f67c889
Rene Vasquez <freddyrenecariasvasquez@gmail.com>
2023-04-19 12:55:02 -0600

    I create the phones file and I added hearder , body and footer

---
+++ src/pages/Phones.tsx
@@ -0,0 +1,15 @@
+import Footer from "../components/GeneralHeader&&Footer/Footer/Footer";
+import Header from "../components/GeneralHeader&&Footer/Header/Header";
+import BodyPhones from "../components/Phones/BodyPhones";
+
+export default function Phones() {
+  return (
+    <>
+      <div className="container text-center">
+        <Header/>
+        <BodyPhones/>
+        <Footer/>
+      </div>
+    </>
+  );
+}
